d19b86c2d9be73877ee0b534fd62674cd3c79ef5
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-21 23:29:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here!
+const scrollContainer = document.getElementById("infi-list");
+let sr_no = 0;
+function addOneItem(){
+	const li = document.createElement("li");
+	li.innerText = `Item ${++sr_no}`;
+	scrollContainer.appendChild(li);
+}
+
+for( let i=0; i<10; i++){
+	addOneItem();
+}
 
